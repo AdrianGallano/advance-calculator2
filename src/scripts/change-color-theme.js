0ee17b3bd8c9000522da4f359f552db5b42b7347
Adrian Gallano <adriangallanoengr@gmail.com>
2023-10-10 17:11:14 +0800

    Add navigation light mode

--- src/scripts/change-color-theme.js
+++ src/scripts/change-color-theme.js
@@ -3,6 +3,9 @@ const calculator = document.getElementsByClassName("calculator")[0];
 const allButtons = document.querySelectorAll("button");
 const history = document.getElementsByClassName("history")[0];
 const displayContainer = document.getElementsByClassName("display")[0];
+const navigation = document.getElementsByClassName("navigation")[0]
+const navigationInputs = document.querySelectorAll(".navigation input")
+
 
 toggle.addEventListener("change", () => {
 
@@ -11,19 +14,26 @@ toggle.addEventListener("change", () => {
     document.body.classList.remove("light-body")
     history.classList.remove("light-history")
     displayContainer.classList.remove("light-display")
-    
+    navigation.classList.remove("light-navigation")
     allButtons.forEach(btn=>{
       btn.classList.remove("light-btn")
     })
-    
+    navigationInputs.forEach(btn => {
+      btn.classList.remove("light-btn")
+    })
   }else{
     calculator.classList.add("light-calc")
     document.body.classList.add("light-body")
     history.classList.add("light-history")
     displayContainer.classList.add("light-display")
+    navigation.classList.add("light-navigation")
     allButtons.forEach(btn=>{
       btn.classList.add("light-btn")
     })
-    
+    console.log(navigationInputs)
+
+    navigationInputs.forEach(btn => {
+      btn.classList.add("light-btn")
+    })
   }
 })
